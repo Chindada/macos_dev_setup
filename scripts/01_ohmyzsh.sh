51fd52c8c1984cb2bd596e70cf78e51b5b5feefc
Tim Hsu@M2MAX-20230322 <maochindada@gmail.com>
2023-03-22 17:57:12 +0800

    fix(ohmyzsh): fix in a new envrionment will not install

--- scripts/01_ohmyzsh.sh
+++ scripts/01_ohmyzsh.sh
@@ -1,15 +1,20 @@
 #!/bin/zsh
 
-echo "Installing oh-my-zsh..."
+NEED_INSTALL=1
 if [[ -d "$HOME/.oh-my-zsh" ]]; then
   echo "oh-my-zsh is already installed, removing it first..."
-  echo -n "Reinstall oh-my-zsh, it will clear current setting.(y/n)?"
+  echo -n "Reinstall will clear current setting, continue(y/n)?"
   read reset
   if [[ $reset == "y" ]]; then
+    echo "Removing oh-my-zsh..."
     rm -rf $HOME/.oh-my-zsh
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
   else
     echo "Skipping oh-my-zsh installation..."
-    return
+    NEED_INSTALL=0
   fi
+fi
+
+if [[ $NEED_INSTALL == 1 ]]; then
+  echo "Installing oh-my-zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
